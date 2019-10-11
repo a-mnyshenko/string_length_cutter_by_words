90c3f76a5ea99db0d65eb813b346343df0903096
flushdev <flushdevelopment@gmail.com>
2019-10-11 17:02:56 +0300

    Some bug fixes

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-var string = document.querySelector('.t').innerText;
+var string = document.querySelector('.t').innerHTML;
 
 const solution = (string, num) => {
   if (string.length < num) {
     return -1;
   }
   var reg = new RegExp(`(?=[^ ]).{1,${num}}(?![^ \.])`, 'g');
-  var re = /(?=[^ ]).{1,50}(?![^ \.])/g;
-  var cuttedArr = string.match(reg);
-  cuttedArr.forEach((element) => {
-    element.replace(/[\t\v\n]/gm, ' ');
-  });
+  var cuttedArr = string.replace(/[\t\v\n\s]+/g, ' ').match(reg);
+
   return { arrTotalParts: cuttedArr.length, arr: cuttedArr };
+  console.log(str3);
 };
 
 console.log(solution(string, 500));
