30b47c9bcc3dfea85f05556d8777b5d71de771d3
flushdev <flushdevelopment@gmail.com>
2019-10-11 17:09:23 +0300

    Delete space at the end of string

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-var string = document.querySelector('.t').innerHTML;
+var string = document.querySelector('.text').innerHTML;
 
 const solution = (string, num) => {
   if (string.length < num) {
     return -1;
   }
   var reg = new RegExp(`(?=[^ ]).{1,${num}}(?![^ \.])`, 'g');
-  var cuttedArr = string.replace(/[\t\v\n\s]+/g, ' ').match(reg);
+  var cuttedArr = string
+    .replace(/[\t\v\n\s]+/g, ' ')
+    .trim()
+    .match(reg);
 
   return { arrTotalParts: cuttedArr.length, arr: cuttedArr };
   console.log(str3);
 };
 
-console.log(solution(string, 500));
+console.log(solution(string, 50));
