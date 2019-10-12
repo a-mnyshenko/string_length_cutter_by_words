c392afbeeae8b5dc1abad2096f0a96071f0c6b71
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-10-13 01:13:12 +0300

    update the solution funciton

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-var S = document.querySelector('.text').innerHTML; //  .innerHTML will give bad formated text for us, i use it for test
+var S = document.querySelector('.text').innerHTML.trim(); //  .innerHTML will give bad formated text for us, i use it for test
+function solution(str, k) {
+let messages = [];
+    let lastSpace, substr1;
 
-const solution = (S, K) => {
-  var reg = new RegExp(`(?=[^ ]).{1,${K}}(?![^ \.])`, 'g');
+    if (str.length > 500 || k > 500 || k < 0) return 'error';
 
-  const format = (str) =>
-    str
-      .replace(/[\t\v\n\s]+/g, ' ')
-      .trim()
-      .match(reg);
+    
 
-  return S.length < K
-    ? -1
-    : {
-        arr: format(S),
-        arrLength: format(S).length,
-      };
-};
-console.log(solution(S, 500));
+    if(str.length < k){
+     messages.push(str.substring(0,k));  
+     return messages;      
+    }
+
+    if(str[k] === ' '){
+        lastSpace = k;
+      
+    }else{
+        substr1 = str.substring(0, k);
+        lastSpace = substr1.lastIndexOf(' ');
+        if(lastSpace === -1) return `error`;
+    }
+        messages.push(str.substring(0,lastSpace));
+        str = str.substr(lastSpace + 1);
+
+    return messages.concat(solution(str,k));
+
+}
+
+
+let K = 12;
+
+solution(S, K).indexOf('error') !== -1 ? console.log(-1) : console.log(solution(S, K).length); 
