f38064d1769af8a2cb587683076ff67f8c68dcf0
flushdev <flushdevelopment@gmail.com>
2019-10-12 13:36:29 +0300

    Change variable names

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var S = document.querySelector('.text').innerHTML;  //  .innerHTML will give to us bad formated text, i use it for test
+var S = document.querySelector('.text').innerHTML; //  .innerHTML will give to us bad formated text, i use it for test
 
 const solution = (S, K) => {
   if (S.length < K) {
     return -1;
   }
   var reg = new RegExp(`(?=[^ ]).{1,${K}}(?![^ \.])`, 'g');
-  var arr = S.replace(/[\t\v\n\s]+/g, ' ').trim().match(reg);
+  var arr = S.replace(/[\t\v\n\s]+/g, ' ')
+    .trim()
+    .match(reg);
   return { arrParts: arr.length, arr: arr };
 };
 console.log(solution(S, 100));
