f63527f5cca2e2298bc93d983ff26ed2a5351090
flushdev <flushdevelopment@gmail.com>
2019-10-13 15:10:52 +0300

    Delete useless code

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var S = document.querySelector('.text').innerHTML; //  .innerHTML will give bad 
 const solution = (S, K) => {
   var reg = new RegExp(`(?=[^ ]).{1,${K}}(?![^ \.])`, 'g');
 
-  return S.length < K ? -1 : S.replace(/[\t\v\n\s]+/g, ' ').trim().match(reg);
-}
+  return S.length < K
+    ? -1
+    : S.replace(/[\t\v\n\s]+/g, ' ')
+        .trim()
+        .match(reg);
+};
 console.log(solution(S, 500));
